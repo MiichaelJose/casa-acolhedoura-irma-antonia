bf0d6c1624bb26e6929bfbdbac44ead2cd854fe7
MiichaelJose <michaeljosecraft1@gmail.com>
2024-04-29 23:56:22 -0300

    alt

--- src/pages/inicio/styles.tsx
+++ src/pages/inicio/styles.tsx
@@ -38,6 +38,7 @@ export const Main = styled.main`
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
 
         > img {
             width: 90%;
