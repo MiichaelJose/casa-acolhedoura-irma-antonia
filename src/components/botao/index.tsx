59da4ed474b8b5bdbc0c280cc4ec909bedbcb083
MiichaelJose <michaeljosedasilva2002@gmail.com>
2024-05-19 21:21:55 -0300

    .

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 }
 
 export default function Button({ children, color, link }: ButtonProps) {
-    return (
-    <Link to={link} backgroundcolor={color} className={"btn"}>{children}</Link>
-    );
+     return <><Link to={link} backgroundcolor={color} className={"btn"}>{children}</Link></>
+        
+    
 }
